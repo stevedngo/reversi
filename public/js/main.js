057e1c78916c77856aa8c45b15c682ed738bd140
Steve Ngo <stevengo@Steves-MBP.T-mobile.com>
2017-06-10 18:38:55 -0700

    DONE!

--- public/js/main.js
+++ public/js/main.js
@@ -309,6 +309,7 @@ var old_board = [
 				];
 
 var my_color = ' ';
+var interval_timer;
 
 
 socket.on('game_update',function(payload){
@@ -343,8 +344,29 @@ socket.on('game_update',function(payload){
 
 
 	$('#my_color').html('<h3 id="my_color">I am '+my_color+'</h3>');
-	$('#my_color').append('<h4>It is '+payload.game.whose_turn+'\'s turn</h4>');
+	$('#my_color').append('<h4>It is '+payload.game.whose_turn+'\'s turn. Elapsed time <span id="elapsed"></span></h4>');
 
+	clearInterval(interval_timer);
+	interval_timer = setInterval(function(last_time){
+		return function(){
+			//Do the work of updating the UI
+			var d = new Date();
+			var elapsedmilli = d.getTime() - last_time;
+			var minutes = Math.floor(elapsedmilli / (60 * 1000));
+			var seconds = Math.floor((elapsedmilli % (60 * 1000))/ 1000);
+
+			if(seconds < 10){
+				$('#elapsed').html(minutes+':0'+seconds);
+			}
+			else{
+				$('#elapsed').html(minutes+':'+seconds);
+			}
+			//
+			//
+			//
+			//
+		}}(payload.game.last_move_time)
+		, 1000);
 
 
 	/* Animate changes to the board */
